ab11212e30116079223718de3041c86904b914aa
Terrance Williams <txwilliams@godaddy.com>
2024-07-02 12:57:39 -0700

    update

--- components/Cart.js
+++ components/Cart.js
@@ -1,27 +1,25 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Typography } from '@mui/material'
 import CartItem from './CartItem';
 
 
-class Cart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { cartItems: [] };
-  }
+function Cart(props) {
 
-  componentDidMount() {
-    fetch('http://localhost:8000/v1/cartitems')
-      .then(response => response.json())
-      .then(data => this.setState({cartItems: data}))
-  }
+  const [cartItems, setCartItems] = useState([]); 
 
-  deleteItem = (id) => {
+  useEffect(async () => {
+    const response = await fetch('http://localhost:8000/v1/cartitems');
+    const json = await response.json();
+    setCartItems([]);
+  }, [])
+
+  function deleteItem(id) {
     fetch(`http://localhost:8000/v1/cartitems/${id}`, { method: 'DELETE' })
       .then(response => {
         if (response.ok) {
-          const newItems = this.state.cartItems.filter(i => i.id != id);
+          const newItems = cartItems.filter(i => i.id != id);
           this.setState({
             cartItems: newItems
           })
@@ -31,45 +29,33 @@ class Cart extends React.Component {
       });
   }
 
-  render() {
-    const totalPrice = this.state.cartItems
-      .map(item => item.quantity * item.price)
-      .reduce((a, b) => a + b, 0)
+  const totalPrice = cartItems
+  .map(item => item.quantity * item.price)
+  .reduce((a, b) => a + b, 0)
 
-    return (
-      <div>
-        <Grid container direction="column" spacing={3}>
-          {this.state.cartItems.map(item =>
-            <Grid item xs={6}>
-              <CartItem
-                product_id={item.product_id}
-                name={item.name}
-                id={item.id}
-                price={item.price}
-                quantity={item.quantity}
-                onRemoveFromCart={this.deleteItem}
-                />
-            </Grid>
-          )}
-        </Grid>
-        <div style={{"padding-top": "20px"}}>
-          <Typography variant="h3">
-            Total: ${totalPrice}
-          </Typography>
-        </div>
+  return (
+    <div>
+      <Grid container direction="column" spacing={3}>
+        {cartItems.map(item =>
+          <Grid item xs={6}>
+            <CartItem
+              product_id={item.product_id}
+              name={item.name}
+              id={item.id}
+              price={item.price}
+              quantity={item.quantity}
+              onRemoveFromCart={this.deleteItem}
+              />
+          </Grid>
+        )}
+      </Grid>
+      <div style={{"padding-top": "20px"}}>
+        <Typography variant="h3">
+          Total: ${totalPrice}
+        </Typography>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-Cart.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string
-};
-
-Cart.defaultProps = {
-  title: 'cart-solution',
-  description: 'A gasket cart app'
-};
 
 export default Cart;
